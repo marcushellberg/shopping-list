517099d0d44c03d74dc920f061c2e9165c3517c2
Marcus Hellberg <marcus@vaadin.com>
2021-01-18 15:36:03 -0800

    Simplified router

--- frontend/routes.ts
+++ frontend/routes.ts
@@ -1,41 +1,27 @@
-import { Flow } from '@vaadin/flow-frontend/Flow';
+import { Route } from "@vaadin/router";
+import "./views/main/main-view";
+import "./views/list/list-view";
 
-const { serverSideRoutes } = new Flow({
-  imports: () => import('../target/frontend/generated-flow-imports'),
-});
-
-export const routes = [
-  // for client-side, place routes below (more info https://vaadin.com/docs/v18/flow/typescript/creating-routes.html)
+export const routes: Route[] = [
   {
-    path: '',
-    component: 'main-view',
-    action: async () => {
-      await import('./views/main/main-view');
-    },
+    path: "",
+    component: "main-view",
     children: [
       {
-        path: '',
-        component: 'list-view',
-        action: async () => {
-          await import('./views/list/list-view');
-        },
+        path: "",
+        component: "list-view",
       },
       {
-        path: 'list',
-        component: 'list-view',
-        action: async () => {
-          await import('./views/list/list-view');
-        },
+        path: "list",
+        component: "list-view",
       },
       {
-        path: 'overview',
-        component: 'overview-view',
+        path: "overview",
+        component: "overview-view",
         action: async () => {
-          await import('./views/overview/overview-view');
+          await import("./views/overview/overview-view");
         },
       },
-      // for server-side, the next magic line sends all unmatched routes:
-      ...serverSideRoutes, // IMPORTANT: this must be the last entry in the array
     ],
   },
 ];
